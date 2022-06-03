47aabd7abd37db508f2fc8e958e8c96d5127fe3a
Mahdere <mtewelign@gmail.com>
2022-06-03 03:50:52 -0700

    nested loop

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * nested loop
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 0 ; x < 10 ; x++)
+	{
+
+		for (y = x + 1 ; y < 10 ; y++)
+		{
+			putchar(x + '0');
+			putchar(y + '0');
+
+			if (x != 8 || y != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
