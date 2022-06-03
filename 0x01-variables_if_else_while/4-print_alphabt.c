784e53b985e964a26d317107a83456fdab741607
Mahdere <mtewelign@gmail.com>
2022-06-03 01:16:00 -0700

    loop with condition statement

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * for loop
+ *
+ * if condition
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lower;
+
+	for (lower = 'a' ; lower <= 'z' ; lower++)
+	{
+		if (lower != e && lower != q)
+		{
+			putchar(lower);
+		}
+	}
+	return (0);
+}
