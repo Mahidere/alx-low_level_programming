38adffad49b59988dd999325dffe2b1a172ac24a
Mahdere <mtewelign@gmail.com>
2022-06-03 02:02:02 -0700

    print from 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * for loop
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0 ; x <= 9 ; x++)
+	{
+		printf("%d", x);
+	}
+	printf("\n");
+	return (0);
+}
