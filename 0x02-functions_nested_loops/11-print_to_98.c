8ce611a5d1c238efb8bacf0fc439167d53d04627
Mahdere <mtewelign@gmail.com>
2022-06-07 08:19:20 -0700

    print upto n

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_to_98 - natural number
+ * @n: variable
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for(; n <=98 ; n++)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else
+	{
+		for (; n >=98 ; n--)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	printf("\n");
+}
