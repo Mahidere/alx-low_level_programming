875169fddbe59ba760fa88e052989213334197eb
Mahdere <mtewelign@gmail.com>
2022-06-07 08:33:56 -0700

    hot fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,7 +8,7 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for(; n <=98 ; n++)
+		for (; n <= 98 ; n++)
 		{
 			printf("%d", n);
 			if (n != 98)
@@ -19,7 +19,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (; n >=98 ; n--)
+		for (; n >= 98 ; n--)
 		{
 			printf("%d", n);
 			if (n != 98)
