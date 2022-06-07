3bb6386066ae167268095e48bdd64a537d78fbed
Mahdere <mtewelign@gmail.com>
2022-06-07 07:31:23 -0700

    rewrote the condition

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,29 +11,28 @@ void times_table(void)
 		for (y = 0 ; y < 10 ; y++)
 		{
 			z = x * y;
-			if (z > 9)
+			if (z < 10)
+			{
+				if (y > 0)
+				{
+					_putchar(' ');
+				}
+				_putchar(z + '0');
+				if (y != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
 			{
 				_putchar((z / 10) + '0');
 				_putchar((z % 10) + '0');
-			}
-			else
-			{
-				_putchar(z + '0');
-			}
-			if (y == 9)
-			{
-				continue;
-			}
-			else if (z > 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
+				if (y != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 		}
 		_putchar('\n');
