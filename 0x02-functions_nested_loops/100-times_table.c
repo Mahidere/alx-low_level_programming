f45d279e8132e81bd1fb9f2c18cad677b7881908
Mahdere <mtewelign@gmail.com>
2022-06-07 09:11:44 -0700

    times table till n

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,54 @@
+#include "main.h"
+/**
+ * print_times_table - function entry
+ * @n: variable
+ */
+void print_times_table(int n)
+{
+	if (n >= 0 && n <= 15)
+	{
+		int x, y, z;
+
+		for (x = 0 ; x <= n ; x++)
+		{
+			for (y = 0 ; y <= n ; y++)
+			{
+				z = x * y;
+				if (z < 10)
+				{
+					if (y > 0)
+					{
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar(z + '0');
+					if (y != n)
+						comma();
+				}
+				else if (z > 9 && z < 100)
+				{
+					_putchar(' ');
+					_putchar((z / 10) + '0');
+					_putchar((z % 10) + '0');
+					if (y != n)
+						comma();
+				}
+				else
+				{
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+					if (y != n)
+						comma();
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
+
+void comma(void)
+{
+	_putchar(',');
+	_putchar(' ');
+}
