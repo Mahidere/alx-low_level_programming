cee8b9add4043c0b6219504d074ff5a601c3f49f
Mahdere <mtewelign@gmail.com>
2022-06-07 07:37:42 -0700

    addition

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * add: adds two integer
+ * add - adds two integer
  * @x: variable
  * @y: variable
  * Return: variable
  */
-int add (int x, int y)
+int add(int x, int y)
 {
 	int z;
 
