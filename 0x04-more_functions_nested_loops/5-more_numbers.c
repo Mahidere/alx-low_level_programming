42165105283f4a1de5b86e239f5b3cec9bba00e7
Mahdere <mtewelign@gmail.com>
2022-06-09 01:33:10 -0700

    print 1-14 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers - print 0-14 10 times
+ */
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0 ; x <= 9 ; x++)
+	{
+		for (y = 0 ; y <= 14; y++)
+		{
+			if (y > 9)
+			{
+				_putchar((y / 10) + '0');
+				_putchar((y % 10) + '0');
+			}
+			else
+			{
+				_putchar(y + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
