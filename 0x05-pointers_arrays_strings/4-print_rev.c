fa487301d457cf7ad3071f4e18799d2cdfc983ca
Mahdere <mtewelign@gmail.com>
2022-06-13 03:15:02 -0700

    print in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - function to print in revers
+ *
+ * @s: variable
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[1])
+	{
+		i++
+	}
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
