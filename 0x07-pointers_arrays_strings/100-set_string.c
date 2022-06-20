bc2c4c22e6ac6d5561c84f00ea7156d6536379f8
Mahdere <mtewelign@gmail.com>
2022-06-20 02:04:22 -0700

    fix

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * set_string - function to set a string
+ *
+ * @s: double pointer
+ * @to: pointer variable
+ */
+void set_string(char **s, char *to)
+{
+	int i;
+
+	while (**s)
+	{
+		for (i = 0 ; i != '\0' ; i++)
+		{
+			to = s;
+		}
+	}
+}
