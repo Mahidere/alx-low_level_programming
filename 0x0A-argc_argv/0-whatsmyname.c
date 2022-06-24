8ec4dff0f52eeaca440cd65eda61520b842146b7
Mahdere <mtewelign@gmail.com>
2022-06-23 23:28:55 -0700

    print the name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the file
+ *
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
