d31dc2ce940a52463e5374ec1777d30c6e693fb6
Mahdere <mtewelign@gmail.com>
2022-06-23 23:36:48 -0700

    number of prog

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - the number of programms
+ *
+ * @argc: number of arguments
+ * @argv: array of argument
+ *
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
